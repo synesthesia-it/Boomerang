e874df1738fb5a3c1f8ecec869dae79123506ee7
Stefano Mondino <stefano.mondino.dev@gmail.com>
2016-10-13 21:21:55 +0200

    Initial Commit

---
+++ Boomerang/Boomerang.h
@@ -0,0 +1,19 @@
+//
+//  Boomerang.h
+//  Boomerang
+//
+//  Created by Stefano Mondino on 13/10/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Boomerang.
+FOUNDATION_EXPORT double BoomerangVersionNumber;
+
+//! Project version string for Boomerang.
+FOUNDATION_EXPORT const unsigned char BoomerangVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Boomerang/PublicHeader.h>
+
+
