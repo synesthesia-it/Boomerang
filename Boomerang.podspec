ec0a5937b38fc484c9307146fd17f6adaebd0c38
Stefano Mondino <stefano.mondino.dev@gmail.com>
2016-11-11 18:07:52 +0100

    first attempt

---
+++ Boomerang.podspec
@@ -0,0 +1,23 @@
+Pod::Spec.new do |s|
+  s.name         = "Boomerang"
+  s.version      = "0.1"
+  s.summary      = "Swift microframework for MVVM"
+  s.description  = <<-DESC
+			Boomerang is a swift microframework for better crossplatform apps 
+                  DESC
+  s.homepage     = "https://github.com/stefanomondino/Boomerang"
+  s.license      = { :type => "MIT", :file => "LICENSE.md" }
+  s.author       = "Stefano Mondino"
+  
+  s.osx.deployment_target = "10.9"
+  s.ios.deployment_target = "8.0"
+  s.tvos.deployment_target = "9.0"
+  s.watchos.deployment_target = "2.0"
+  
+  s.source       = { :git => "https://github.com/stefanomondino/Boomerang.git", :tag => "#{s.version}" }
+  s.source_files = "Boomerang/*.{swift,h,m}"
+  s.ios.source_files = "Boomerang/UIKit/*.{swift}"
+  s.module_map = "Boomerang/module.modulemap"
+  
+  s.dependency 'ReactiveCocoa', '~> 5.0.0-alpha.3'
+end
--- Boomerang/UIKit/UITableView.swift
+++ Boomerang/UIKit/UITableView.swift
@@ -1,91 +1,91 @@
+////
+////  UITableView.swift
+////  Boomerang
+////
+////  Created by Stefano Mondino on 02/11/16.
+////
+////
 //
-//  UITableView.swift
-//  Boomerang
-//
-//  Created by Stefano Mondino on 02/11/16.
+//import UIKit
 //
 //
-
-import UIKit
-
-
-private class ViewModelTableViewDataSource : NSObject, UITableViewDataSource {
-    weak var viewModel: ListViewModel?
-    weak var modelStructure:ModelStructureType
-    init (viewModel:ListViewModel,  modelStructure: ModelStructureType) {
-        super.init()
-        self.viewModel = viewModel
-        self.modelStructure = modelStructure
-    }
-    
-    @objc public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let viewModel:ItemViewModelType? = self.viewModel?.viewModelAtIndex(indexPath)
-        let cell = tableView.dequeueReusableCell(withIdentifier: viewModel?.itemIdentifier.name ?? defaultListIdentifier, for: indexPath)
-        (cell as? ViewModelBindable)?.bindViewModel(viewModel)
-        return cell
-    }
-    public func numberOfSections(in tableView: UITableView) -> Int {
-        let count =  self.viewModel?.models.value.children?.count ?? 1
-        return count
-    }
-    
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        var count =  self.viewModel?.models.value.children?[section].models?.count
-        count =  count ?? self.viewModel?.models.value.models?.count
-        return count ?? 0
-    }
-    
-    
-//
-//    @objc public func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-//        let model = self.viewModel?.models.value.children?[indexPath.section].sectionModel
-//        if nil != model {
-//            
-//            let vm =  self.viewModel?.itemViewModel(model!)
-//            if (vm != nil) {
-//                let cell = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: vm!.itemIdentifier.name, for: indexPath)
-//                (cell as? ViewModelBindable)?.bindViewModel(vm)
-//                return cell
-//            }
-//        }
-//        
-//        return UICollectionViewCell()
+//private class ViewModelTableViewDataSource : NSObject, UITableViewDataSource {
+//    weak var viewModel: ListViewModel?
+//    weak var modelStructure:ModelStructureType
+//    init (viewModel:ListViewModel,  modelStructure: ModelStructureType) {
+//        super.init()
+//        self.viewModel = viewModel
+//        self.modelStructure = modelStructure
 //    }
-    
-    
-}
-
-
-
-public extension ListViewModelType  {
-    var tableViewDataSource:UITableViewDataSource {
-        return ViewModelTableViewDataSource(viewModel: self, modelStructure:self.models)
-    }
-    
-
-
-}
-
-extension UITableView : ViewModelBindable {
-    public func bindViewModel(_ viewModel: ViewModelType?) {
-        guard let vm = viewModel as? ListViewModelType else {
-            return
-        }
-        let tableView = self
-        _ = vm.listIdentifiers().map { $0.name}.reduce("", { (_, value) in
-            tableView.register(UINib(nibName: value.name, bundle: nil), forCellReuseIdentifier: value)
-            return ""
-        })
-        _ = (vm as? ListViewModelTypeHeaderable)?.headerIdentifiers().reduce("", { (_, value) in
-            tableView.register(UINib(nibName: value.name, bundle: nil), forHeaderFooterViewReuseIdentifier: value.name)
-            return ""
-        })
-        let dataSource = vm.tableViewDataSource
-        self.dataSource = dataSource
-      
-        vm.resultsCount.producer.startWithResult {[weak self] _ in
-            self?.reloadData()
-            dataSource
-        }
-    }
-}
+//    
+//    @objc public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let viewModel:ItemViewModelType? = self.viewModel?.viewModelAtIndex(indexPath)
+//        let cell = tableView.dequeueReusableCell(withIdentifier: viewModel?.itemIdentifier.name ?? defaultListIdentifier, for: indexPath)
+//        (cell as? ViewModelBindable)?.bindViewModel(viewModel)
+//        return cell
+//    }
+//    public func numberOfSections(in tableView: UITableView) -> Int {
+//        let count =  self.viewModel?.models.value.children?.count ?? 1
+//        return count
+//    }
+//    
+//    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        var count =  self.viewModel?.models.value.children?[section].models?.count
+//        count =  count ?? self.viewModel?.models.value.models?.count
+//        return count ?? 0
+//    }
+//    
+//    
+////
+////    @objc public func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+////        let model = self.viewModel?.models.value.children?[indexPath.section].sectionModel
+////        if nil != model {
+////            
+////            let vm =  self.viewModel?.itemViewModel(model!)
+////            if (vm != nil) {
+////                let cell = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: vm!.itemIdentifier.name, for: indexPath)
+////                (cell as? ViewModelBindable)?.bindViewModel(vm)
+////                return cell
+////            }
+////        }
+////        
+////        return UICollectionViewCell()
+////    }
+//    
+//    
+//}
+//
+//
+//
+//public extension ListViewModelType  {
+//    var tableViewDataSource:UITableViewDataSource {
+//        return ViewModelTableViewDataSource(viewModel: self, modelStructure:self.models)
+//    }
+//    
+//
+//
+//}
+//
+//extension UITableView : ViewModelBindable {
+//    public func bindViewModel(_ viewModel: ViewModelType?) {
+//        guard let vm = viewModel as? ListViewModelType else {
+//            return
+//        }
+//        let tableView = self
+//        _ = vm.listIdentifiers().map { $0.name}.reduce("", { (_, value) in
+//            tableView.register(UINib(nibName: value.name, bundle: nil), forCellReuseIdentifier: value)
+//            return ""
+//        })
+//        _ = (vm as? ListViewModelTypeHeaderable)?.headerIdentifiers().reduce("", { (_, value) in
+//            tableView.register(UINib(nibName: value.name, bundle: nil), forHeaderFooterViewReuseIdentifier: value.name)
+//            return ""
+//        })
+//        let dataSource = vm.tableViewDataSource
+//        self.dataSource = dataSource
+//      
+//        vm.resultsCount.producer.startWithResult {[weak self] _ in
+//            self?.reloadData()
+//            dataSource
+//        }
+//    }
+//}
