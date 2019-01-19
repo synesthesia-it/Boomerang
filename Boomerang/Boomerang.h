c75676b40944c59205fbdb12ef1c432766bbc6f7
Stefano Mondino <puntoste@gmail.com>
2019-01-19 14:56:05 +0100

    Initial Commit

---
+++ Boomerang/Boomerang.h
@@ -0,0 +1,19 @@
+//
+//  Boomerang.h
+//  Boomerang
+//
+//  Created by Stefano Mondino on 19/01/2019.
+//  Copyright © 2019 Synesthesia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Boomerang.
+FOUNDATION_EXPORT double BoomerangVersionNumber;
+
+//! Project version string for Boomerang.
+FOUNDATION_EXPORT const unsigned char BoomerangVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Boomerang/PublicHeader.h>
+
+
