b017c4cf12d24785d520e999c59671057002e222
Stefano Mondino <stefano.mondino.dev@gmail.com>
2019-10-22 21:02:09 +0200

    Initial Commit

---
+++ NewArch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewArch
+//
+//  Created by Stefano Mondino on 22/10/2019.
+//  Copyright © 2019 Synesthesia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
