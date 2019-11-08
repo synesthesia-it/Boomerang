0dd15d8c9b065251591fc491ac5f65f11be8cd44
Stefano Mondino <stefano.mondino.dev@gmail.com>
2019-11-08 23:15:53 +0100

    refactor

--- Demo/DependencyContainer.swift
+++ Demo/DependencyContainer.swift
@@ -1,56 +1,64 @@
-    import Foundation
-    import Boomerang
-    
-    protocol AppDependencyContainer  {
-        var routeFactory: RouteFactory { get }
-        var viewFactory: ViewFactory { get }
-        var collectionViewCellFactory: CollectionViewCellFactory { get }
-        var viewControllerFactory: ViewControllerFactory { get }
-    }
-    
-    enum DependencyContainerKeys: CaseIterable, Hashable {
-        case routeFactory
-        case collectionViewCellFactory
-        case viewFactory
-        case viewControllerFactory
-    }
-    
-    
-    class DefaultAppDependencyContainer: AppDependencyContainer, DependencyContainer {
-        typealias Key = DependencyContainerKeys
+//
+//  DependencyContainer.swift
+//  NewArch
+//
+//  Created by Stefano Mondino on 22/10/2019.
+//  Copyright © 2019 Synesthesia. All rights reserved.
+//
 
-        var container: [Key: () -> Any ] = [:]
-        
-        var routeFactory: RouteFactory { self[.routeFactory] }
-        var viewFactory: ViewFactory { self[.viewFactory] }
-        var viewControllerFactory: ViewControllerFactory { self[.viewControllerFactory] }
-        var collectionViewCellFactory: CollectionViewCellFactory { self[.collectionViewCellFactory] }
-        
-        init() {
-            self.register(for: .routeFactory) { MainRouteFactory(container: self) }
-            self.register(for: .viewFactory) { MainViewFactory()}
-            self.register(for: .collectionViewCellFactory) { MainCollectionViewCellFactory(viewFactory: self.viewFactory) }
-            self.register(for: .viewControllerFactory) { DefaultViewControllerFactory(container: self) }
-        }
-        
-        subscript<T>(index: Key) -> T {
-            guard let element: T = resolve(index) else {
-                fatalError("No dependency found for \(index)")
-            }
-            return element
-        }
+import Foundation
+import Boomerang
+
+protocol AppDependencyContainer  {
+    var routeFactory: RouteFactory { get }
+    var viewFactory: ViewFactory { get }
+    var collectionViewCellFactory: CollectionViewCellFactory { get }
+    var viewControllerFactory: ViewControllerFactory { get }
+}
+
+enum DependencyContainerKeys: CaseIterable, Hashable {
+    case routeFactory
+    case collectionViewCellFactory
+    case viewFactory
+    case viewControllerFactory
+}
+
+
+class DefaultAppDependencyContainer: AppDependencyContainer, DependencyContainer {
+    typealias Key = DependencyContainerKeys
+    
+    var container: [Key: () -> Any ] = [:]
+    
+    var routeFactory: RouteFactory { self[.routeFactory] }
+    var viewFactory: ViewFactory { self[.viewFactory] }
+    var viewControllerFactory: ViewControllerFactory { self[.viewControllerFactory] }
+    var collectionViewCellFactory: CollectionViewCellFactory { self[.collectionViewCellFactory] }
+    
+    init() {
+        self.register(for: .routeFactory) { MainRouteFactory(container: self) }
+        self.register(for: .viewFactory) { MainViewFactory()}
+        self.register(for: .collectionViewCellFactory) { MainCollectionViewCellFactory(viewFactory: self.viewFactory) }
+        self.register(for: .viewControllerFactory) { DefaultViewControllerFactory(container: self) }
     }
     
-    ///Convert in Test
-    extension DefaultAppDependencyContainer {
-        func testAll() {
+    subscript<T>(index: Key) -> T {
+        guard let element: T = resolve(index) else {
+            fatalError("No dependency found for \(index)")
+        }
+        return element
+    }
+}
+
+///Convert in Test, this is temporary
+extension DefaultAppDependencyContainer {
+    func testAll() {
+        
+        DependencyContainerKeys.allCases.forEach {
+            //expect no throw
+            let v: Any = self[$0]
+            print(v)
             
-            DependencyContainerKeys.allCases.forEach {
-                //expect no throw
-                let v: Any = self[$0]
-                print(v)
-                
-            }
         }
     }
-    
+}
+
