4328e82c58d4b087bc3586a6d7d51b68faaaafa2
Stefano Mondino <stefano.mondino.dev@gmail.com>
2019-11-20 23:10:56 +0100

    exposing size calculator

--- Sources/UIKit/UICollectionView/CollectionViewDelegate.swift
+++ Sources/UIKit/UICollectionView/CollectionViewDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 
 open class CollectionViewDelegate: NSObject, UICollectionViewDelegateFlowLayout {
     public typealias Select = (IndexPath) -> Void
-
+    
     public let viewModel: ListViewModel
     public let dataSource: CollectionViewDataSource
     public var cellFactory: CollectionViewCellFactory {
@@ -22,33 +22,33 @@ open class CollectionViewDelegate: NSObject, UICollectionViewDelegateFlowLayout 
         self.viewModel = viewModel
         self.dataSource = dataSource
     }
-
+    
     public static var defaultInsets: UIEdgeInsets?
     public static var defaultLineSpacing: CGFloat?
     public static var defaultItemSpacing: CGFloat?
     public static var defaultItemsPerLine: Int?
-
+    
     public typealias Size = (UICollectionView, IndexPath, String?) -> CGSize
     public typealias Spacing = (UICollectionView, Int) -> CGFloat
     public typealias Insets = (UICollectionView, Int) -> UIEdgeInsets
-
+    
     private var didSelect: Select = { _ in }
     private var didDeselect: Select = { _ in }
-
+    
     private lazy var size: Size = {[weak self] collectionView, indexPath, type in
         guard let self = self,
             let items = CollectionViewDelegate.defaultItemsPerLine else {
-            return (collectionView.collectionViewLayout as? UICollectionViewFlowLayout)?.itemSize ?? .zero
+                return (collectionView.collectionViewLayout as? UICollectionViewFlowLayout)?.itemSize ?? .zero
         }
-
+        
         return UICollectionViewSizeCalculator(collectionView: collectionView, viewModel: self.viewModel, factory: self.cellFactory).automaticSizeForItem(at: indexPath, type: type, itemsPerLine: items)
-
+        
     }
-
+    
     private var insets: Insets = { collectionView, section in
         return CollectionViewDelegate.defaultInsets ?? (collectionView.collectionViewLayout as? UICollectionViewFlowLayout)?.sectionInset ?? .zero
     }
-
+    
     private var lineSpacing: Spacing = { collectionView, section in
         return CollectionViewDelegate.defaultLineSpacing ?? (collectionView.collectionViewLayout as? UICollectionViewFlowLayout)?.minimumLineSpacing ?? 0
     }
@@ -56,7 +56,7 @@ open class CollectionViewDelegate: NSObject, UICollectionViewDelegateFlowLayout 
         return CollectionViewDelegate.defaultItemSpacing ??
             (collectionView.collectionViewLayout as? UICollectionViewFlowLayout)?.minimumInteritemSpacing ?? 0
     }
-
+    
     open func withSize(size: @escaping Size) -> Self {
         self.size = size
         return self
@@ -69,7 +69,7 @@ open class CollectionViewDelegate: NSObject, UICollectionViewDelegateFlowLayout 
         self.itemSpacing = itemSpacing
         return self
     }
-
+    
     open func withInsets(insets: @escaping Insets) -> Self {
         self.insets = insets
         return self
@@ -78,27 +78,29 @@ open class CollectionViewDelegate: NSObject, UICollectionViewDelegateFlowLayout 
         self.didSelect = select
         return self
     }
-
+    
     open func withDeselect(deselect: @escaping Select) -> Self {
         self.didDeselect = deselect
         return self
     }
-
+    open func sizeCalculator(for collectionView: UICollectionView) -> UICollectionViewSizeCalculator {
+        return UICollectionViewSizeCalculator(collectionView: collectionView,
+                                              viewModel: self.viewModel,
+                                              factory: self.cellFactory)
+    }
     open func withItemsPerLine(itemsPerLine: Int?) -> Self {
         self.size = {[weak self] collectionView, indexPath, type in
             guard let self = self,
                 let itemsPerLine = itemsPerLine else {
-                return (collectionView.collectionViewLayout as? UICollectionViewFlowLayout)?.itemSize ?? .zero
+                    return (collectionView.collectionViewLayout as? UICollectionViewFlowLayout)?.itemSize ?? .zero
             }
-
-            return UICollectionViewSizeCalculator(collectionView: collectionView,
-                                                  viewModel: self.viewModel,
-                                                  factory: self.cellFactory)
+            
+            return self.sizeCalculator(for: collectionView)
                 .automaticSizeForItem(at: indexPath, type: type, itemsPerLine: itemsPerLine)
         }
         return self
     }
-
+    
     open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return size(collectionView, indexPath, nil)
     }
@@ -108,19 +110,19 @@ open class CollectionViewDelegate: NSObject, UICollectionViewDelegateFlowLayout 
     open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
         return size(collectionView, IndexPath(item: 0, section: section), UICollectionView.elementKindSectionFooter)
     }
-
+    
     open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return insets(collectionView, section)
     }
-
+    
     open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return lineSpacing(collectionView, section)
     }
-
+    
     open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return itemSpacing(collectionView, section)
     }
-
+    
     open func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         self.didSelect(indexPath)
     }
