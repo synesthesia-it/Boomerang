e25850a1c7b51f6dd797398d8d64811ee4b0bd0f
Stefano Mondino <stefano.mondino.dev@gmail.com>
2021-12-20 11:29:33 +0100

    removed useless files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@ All notable changes to this project will be documented in this file
 ---
 
 ## Unreleased
+- Improved test utilities for RxStateMachine
 
 ## 6.6.0
 
--- Demo/Model/DataSource/ImageDataSource.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ImageDataSource.swift
-//  Demo
-//
-//  Created by Stefano Mondino on 24/10/2019.
-//  Copyright © 2019 Synesthesia. All rights reserved.
-//
-
-import Foundation
